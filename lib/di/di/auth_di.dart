89f1fdcc4c643d43b8236f65371d83b9981a426c
Xoldarov Temur <temuruz2003@gmail.com>
2023-10-13 14:24:05 +0500

    login

--- lib/di/di/auth_di.dart
+++ lib/di/di/auth_di.dart
@@ -1,27 +1,25 @@
-
 import 'package:get_it/get_it.dart';
 import 'package:hackathon/di/di/feature_di.dart';
+import 'package:hackathon/presentation/screens/auth/bloc/auth/login/login_bloc.dart';
+import 'package:hackathon/presentation/screens/auth/bloc/auth/register/register_bloc.dart';
 
 class AuthDi extends FeatureDi {
   @override
-  void initDataSources(GetIt injector) {
-  }
+  void initDataSources(GetIt injector) {}
 
   @override
-  void initMappers(GetIt injector) {
-  }
+  void initMappers(GetIt injector) {}
 
   @override
-  void initRepositories(GetIt injector) {
-  }
+  void initRepositories(GetIt injector) {}
 
   @override
-  void initUseCases(GetIt injector) {
-  }
+  void initUseCases(GetIt injector) {}
 
   @override
   void initBlocks(GetIt injector) {
-
+    injector.registerFactory<LoginBloc>(() => LoginBloc());
+    injector.registerFactory<RegisterBloc>(() => RegisterBloc());
   }
 
   @override
